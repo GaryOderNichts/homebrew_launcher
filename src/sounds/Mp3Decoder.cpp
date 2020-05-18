080252a973f2eed410a52c79af46465c337d3553
GaryOderNichts <naj02@gmx.de>
2020-05-18 23:17:06 +0200

    Revert "remove libmad"
    This reverts commit 17cc72df7de346f3a528ca5f24273506f6a6af2a.

--- src/sounds/Mp3Decoder.cpp
+++ src/sounds/Mp3Decoder.cpp
@@ -35,94 +35,93 @@
 Mp3Decoder::Mp3Decoder(const char * filepath)
 	: SoundDecoder(filepath)
 {
-	// SoundType = SOUND_MP3;
-	// ReadBuffer = NULL;
-	// mad_timer_reset(&Timer);
-	// mad_stream_init(&Stream);
-	// mad_frame_init(&Frame);
-	// mad_synth_init(&Synth);
+	SoundType = SOUND_MP3;
+	ReadBuffer = NULL;
+	mad_timer_reset(&Timer);
+	mad_stream_init(&Stream);
+	mad_frame_init(&Frame);
+	mad_synth_init(&Synth);
 
-	// if(!file_fd)
-	// 	return;
+	if(!file_fd)
+		return;
 
-	// OpenFile();
+	OpenFile();
 }
 
 Mp3Decoder::Mp3Decoder(const u8 * snd, int len)
 	: SoundDecoder(snd, len)
 {
-	// SoundType = SOUND_MP3;
-	// ReadBuffer = NULL;
-	// mad_timer_reset(&Timer);
-	// mad_stream_init(&Stream);
-	// mad_frame_init(&Frame);
-	// mad_synth_init(&Synth);
+	SoundType = SOUND_MP3;
+	ReadBuffer = NULL;
+	mad_timer_reset(&Timer);
+	mad_stream_init(&Stream);
+	mad_frame_init(&Frame);
+	mad_synth_init(&Synth);
 
-	// if(!file_fd)
-	// 	return;
+	if(!file_fd)
+		return;
 
-	// OpenFile();
+	OpenFile();
 }
 
 Mp3Decoder::~Mp3Decoder()
 {
-	// ExitRequested = true;
-	// while(Decoding)
-	// 	usleep(100);
+	ExitRequested = true;
+	while(Decoding)
+		usleep(100);
 
-	// mad_synth_finish(&Synth);
-	// mad_frame_finish(&Frame);
-	// mad_stream_finish(&Stream);
+	mad_synth_finish(&Synth);
+	mad_frame_finish(&Frame);
+	mad_stream_finish(&Stream);
 
-	// if(ReadBuffer)
-	// 	free(ReadBuffer);
-	// ReadBuffer = NULL;
+	if(ReadBuffer)
+		free(ReadBuffer);
+	ReadBuffer = NULL;
 }
 
 void Mp3Decoder::OpenFile()
 {
-	// GuardPtr = NULL;
-	// ReadBuffer = (u8 *) memalign(32, SoundBlockSize*SoundBlocks);
-	// if(!ReadBuffer)
-	// {
-	// 	if(file_fd)
-	// 		delete file_fd;
-	// 	file_fd = NULL;
-	// 	return;
-	// }
+	GuardPtr = NULL;
+	ReadBuffer = (u8 *) memalign(32, SoundBlockSize*SoundBlocks);
+	if(!ReadBuffer)
+	{
+		if(file_fd)
+			delete file_fd;
+		file_fd = NULL;
+		return;
+	}
 
-	// u8 dummybuff[4096];
-	// int ret = Read(dummybuff, 4096, 0);
-	// if(ret <= 0)
-	// {
-	// 	if(file_fd)
-	// 		delete file_fd;
-	// 	file_fd = NULL;
-	// 	return;
-	// }
+	u8 dummybuff[4096];
+	int ret = Read(dummybuff, 4096, 0);
+	if(ret <= 0)
+	{
+		if(file_fd)
+			delete file_fd;
+		file_fd = NULL;
+		return;
+	}
 
-	// SampleRate = (u32) Frame.header.samplerate;
-	// Format = ((MAD_NCHANNELS(&Frame.header) == 2) ? (FORMAT_PCM_16_BIT | CHANNELS_STEREO) : (FORMAT_PCM_16_BIT | CHANNELS_MONO));
-	// Rewind();
+	SampleRate = (u32) Frame.header.samplerate;
+	Format = ((MAD_NCHANNELS(&Frame.header) == 2) ? (FORMAT_PCM_16_BIT | CHANNELS_STEREO) : (FORMAT_PCM_16_BIT | CHANNELS_MONO));
+	Rewind();
 }
 
 int Mp3Decoder::Rewind()
 {
-	// mad_synth_finish(&Synth);
-	// mad_frame_finish(&Frame);
-	// mad_stream_finish(&Stream);
-	// mad_timer_reset(&Timer);
-	// mad_stream_init(&Stream);
-	// mad_frame_init(&Frame);
-	// mad_synth_init(&Synth);
-	// SynthPos = 0;
-	// GuardPtr = NULL;
+	mad_synth_finish(&Synth);
+	mad_frame_finish(&Frame);
+	mad_stream_finish(&Stream);
+	mad_timer_reset(&Timer);
+	mad_stream_init(&Stream);
+	mad_frame_init(&Frame);
+	mad_synth_init(&Synth);
+	SynthPos = 0;
+	GuardPtr = NULL;
 
-	// if(!file_fd)
-	// 	return -1;
+	if(!file_fd)
+		return -1;
 
-	// return SoundDecoder::Rewind();
-	return 0;
+	return SoundDecoder::Rewind();
 }
 
 static inline s16 FixedToShort(mad_fixed_t Fixed)
@@ -139,80 +138,80 @@ static inline s16 FixedToShort(mad_fixed_t Fixed)
 
 int Mp3Decoder::Read(u8 * buffer, int buffer_size, int pos)
 {
-	// if(!file_fd)
-	// 	return -1;
+	if(!file_fd)
+		return -1;
 
-	// if(Format == (FORMAT_PCM_16_BIT | CHANNELS_STEREO))
-	// 	buffer_size &= ~0x0003;
-	// else
-	// 	buffer_size &= ~0x0001;
+	if(Format == (FORMAT_PCM_16_BIT | CHANNELS_STEREO))
+		buffer_size &= ~0x0003;
+	else
+		buffer_size &= ~0x0001;
 
-	// u8 * write_pos = buffer;
-	// u8 * write_end = buffer+buffer_size;
+	u8 * write_pos = buffer;
+	u8 * write_end = buffer+buffer_size;
 
-	// while(1)
-	// {
-	// 	while(SynthPos < Synth.pcm.length)
-	// 	{
-	// 		if(write_pos >= write_end)
-	// 			return write_pos-buffer;
+	while(1)
+	{
+		while(SynthPos < Synth.pcm.length)
+		{
+			if(write_pos >= write_end)
+				return write_pos-buffer;
 
-	// 		*((s16 *) write_pos) = FixedToShort(Synth.pcm.samples[0][SynthPos]);
-	// 		write_pos += 2;
+			*((s16 *) write_pos) = FixedToShort(Synth.pcm.samples[0][SynthPos]);
+			write_pos += 2;
 
-	// 		if(MAD_NCHANNELS(&Frame.header) == 2)
-	// 		{
-	// 			*((s16 *) write_pos) = FixedToShort(Synth.pcm.samples[1][SynthPos]);
-	// 			write_pos += 2;
-	// 		}
-	// 		SynthPos++;
-	// 	}
+			if(MAD_NCHANNELS(&Frame.header) == 2)
+			{
+				*((s16 *) write_pos) = FixedToShort(Synth.pcm.samples[1][SynthPos]);
+				write_pos += 2;
+			}
+			SynthPos++;
+		}
 
-	// 	if(Stream.buffer == NULL || Stream.error == MAD_ERROR_BUFLEN)
-	// 	{
-	// 		u8 * ReadStart = ReadBuffer;
-	// 		int ReadSize = SoundBlockSize*SoundBlocks;
-	// 		int Remaining = 0;
+		if(Stream.buffer == NULL || Stream.error == MAD_ERROR_BUFLEN)
+		{
+			u8 * ReadStart = ReadBuffer;
+			int ReadSize = SoundBlockSize*SoundBlocks;
+			int Remaining = 0;
 
-	// 		if(Stream.next_frame != NULL)
-	// 		{
-	// 			Remaining = Stream.bufend - Stream.next_frame;
-	// 			memmove(ReadBuffer, Stream.next_frame, Remaining);
-	// 			ReadStart += Remaining;
-	// 			ReadSize -= Remaining;
-	// 		}
+			if(Stream.next_frame != NULL)
+			{
+				Remaining = Stream.bufend - Stream.next_frame;
+				memmove(ReadBuffer, Stream.next_frame, Remaining);
+				ReadStart += Remaining;
+				ReadSize -= Remaining;
+			}
 
-	// 		ReadSize = file_fd->read(ReadStart, ReadSize);
-	// 		if(ReadSize <= 0)
-	// 		{
-	// 			GuardPtr = ReadStart;
-	// 			memset(GuardPtr, 0, MAD_BUFFER_GUARD);
-	// 			ReadSize = MAD_BUFFER_GUARD;
-	// 		}
+			ReadSize = file_fd->read(ReadStart, ReadSize);
+			if(ReadSize <= 0)
+			{
+				GuardPtr = ReadStart;
+				memset(GuardPtr, 0, MAD_BUFFER_GUARD);
+				ReadSize = MAD_BUFFER_GUARD;
+			}
 
-	// 		CurPos += ReadSize;
-	// 		mad_stream_buffer(&Stream, ReadBuffer, Remaining+ReadSize);
-	// 	}
+			CurPos += ReadSize;
+			mad_stream_buffer(&Stream, ReadBuffer, Remaining+ReadSize);
+		}
 
-	// 	if(mad_frame_decode(&Frame,&Stream))
-	// 	{
-	// 		if(MAD_RECOVERABLE(Stream.error))
-	// 		{
-	// 		  if(Stream.error != MAD_ERROR_LOSTSYNC || !GuardPtr)
-	// 			continue;
-	// 		}
-	// 		else
-	// 		{
-	// 			if(Stream.error != MAD_ERROR_BUFLEN)
-	// 				return -1;
-	// 			else if(Stream.error == MAD_ERROR_BUFLEN && GuardPtr)
-	// 				return -1;
-	// 		}
-	// 	}
+		if(mad_frame_decode(&Frame,&Stream))
+		{
+			if(MAD_RECOVERABLE(Stream.error))
+			{
+			  if(Stream.error != MAD_ERROR_LOSTSYNC || !GuardPtr)
+				continue;
+			}
+			else
+			{
+				if(Stream.error != MAD_ERROR_BUFLEN)
+					return -1;
+				else if(Stream.error == MAD_ERROR_BUFLEN && GuardPtr)
+					return -1;
+			}
+		}
 
-	// 	mad_timer_add(&Timer,Frame.header.duration);
-	// 	mad_synth_frame(&Synth,&Frame);
-	// 	SynthPos = 0;
-	// }
+		mad_timer_add(&Timer,Frame.header.duration);
+		mad_synth_frame(&Synth,&Frame);
+		SynthPos = 0;
+	}
 	return 0;
 }
